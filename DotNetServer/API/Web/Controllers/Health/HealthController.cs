ed009833d3296e64f5d51ec47f96f4d7005c208b
Kiko <kristiqnenchevv@gmail.com>
2023-05-26 14:13:51 +0300

    Update References and add HealthChecks

---
+++ DotNetServer/API/Web/Controllers/Health/HealthController.cs
@@ -0,0 +1,50 @@
+﻿namespace Web.Controllers.Health
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+    using Swashbuckle.AspNetCore.Annotations;
+
+    using Models.HealthCheck;
+
+    public class HealthController : ApiController
+    {
+        private readonly HealthCheckService _healthCheckService;
+
+        public HealthController(HealthCheckService healthCheckService)
+        {
+            _healthCheckService = healthCheckService;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        [SwaggerResponse(200, "Success")]
+        [SwaggerResponse(400, "Invalid or missing data supplied")]
+        [SwaggerResponse(500, "Internal Server error")]
+        [SwaggerOperation("Controller for testing all dependencies.", "Test all dependencies using tasks.")]
+        public async Task<IActionResult> CheckHealth()
+        {
+            var result = await _healthCheckService.CheckHealthAsync();
+            var healthCheckDto = new HealthResult
+            {
+                Entries = result.Entries.Select(e => new HealthEntry
+                {
+                    Name = e.Key,
+                    Status = e.Value.Status.ToString(),
+                    Duration = e.Value.Duration,
+                    Exception = e.Value.Exception?.Message
+                }).ToList(),
+                Status = result.Status.ToString(),
+                TotalChecks = result.Entries.Count
+            };
+
+            if (result.Status == HealthStatus.Healthy)
+            {
+                return Ok(healthCheckDto);
+            }
+
+            return StatusCode(500, healthCheckDto);
+        }
+    }
+}
---
+++ DotNetServer/API/Web/Startup.cs
@@ -0,0 +1,57 @@
+﻿namespace Web
+{
+    using System.Reflection;
+
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Routing;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class Startup
+    {
+        public static IServiceCollection AddWeb(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddHttpContextAccessor();
+            services.AddControllers().AddApplicationPart(Assembly.GetExecutingAssembly()).AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
+            });
+
+            return services;
+        }
+
+        public static IServiceCollection AddConfigurations(this IServiceCollection services, IWebHostBuilder hostBulder, IWebHostEnvironment env)
+        {
+            hostBulder.ConfigureAppConfiguration(config =>
+            {
+                config.SetBasePath(Directory.GetCurrentDirectory());
+                config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+                config.AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+                config.AddEnvironmentVariables();
+                config.Build();
+            });
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseWeb(this IApplicationBuilder builder)
+        {
+            builder
+                    .UseStaticFiles()
+                    .UseHttpsRedirection()
+                    .UseRouting()
+                    .UseAuthentication()
+                    .UseAuthorization();
+
+            return builder;
+        }
+
+        public static IEndpointRouteBuilder MapEndpoints(this IEndpointRouteBuilder builder)
+        {
+            builder.MapControllers();
+
+            return builder;
+        }
+    }
+}
