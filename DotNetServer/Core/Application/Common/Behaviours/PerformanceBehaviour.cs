210624735bb402b1f6c6989967baeaf9dc6cbe2e
Kiko <kristiqnenchevv@gmail.com>
2023-06-05 12:16:28 +0300

    Add fluent validation and Automapper

---
+++ DotNetServer/Core/Application/Common/Behaviours/PerformanceBehaviour.cs
@@ -0,0 +1,47 @@
+﻿namespace Application.Common.Behaviours
+{
+    using System.Diagnostics;
+
+    using Microsoft.Extensions.Logging;
+
+    using MediatR;
+
+    using Application.Interfaces;
+
+    public class PerformanceBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : notnull
+    {
+        private readonly Stopwatch _timer;
+        private readonly ILogger<TRequest> _logger;
+        private readonly IUser _user;
+
+        public PerformanceBehaviour(ILogger<TRequest> logger, IUser user)
+        {
+            _logger = logger;
+            _user = user;
+            _timer = new Stopwatch();
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            _timer.Start();
+
+            var response = await next();
+
+            _timer.Stop();
+
+            var elapsedMilliseconds = _timer.ElapsedMilliseconds;
+
+            if (elapsedMilliseconds > 500)
+            {
+                var requestName = typeof(TRequest).Name;
+                var userId = _user.Id ?? string.Empty;
+                string? email = _user.Id;
+
+                _logger.LogWarning("Todo Service Long Running Request: {Name} ({ElapsedMilliseconds} milliseconds) {@UserId} {@Email} {@Request}",
+                    requestName, elapsedMilliseconds, userId, email, request);
+            }
+
+            return response;
+        }
+    }
+}
--- DotNetServer/Core/Application/Startup.cs
+++ DotNetServer/Core/Application/Startup.cs
@@ -4,11 +4,28 @@
 
     using Microsoft.Extensions.DependencyInjection;
 
+    using FluentValidation;
+
+    using Application.Common.Behaviours;
+
+    using MediatR;
+
     public static class Startup
     {
         public static void AddApplication(this IServiceCollection services)
         {
+            services.AddAutoMapper();
+            services.AddValidators();
             services.AddMediator();
+        }
+
+        private static void AddAutoMapper(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        }
+        private static void AddValidators(this IServiceCollection services)
+        {
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
         }
 
         private static void AddMediator(this IServiceCollection services)
@@ -16,6 +33,8 @@
             services.AddMediatR(cfg =>
             {
                 cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+                cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+                cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(PerformanceBehaviour<,>));
             });
         }
     }
