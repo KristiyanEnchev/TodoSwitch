c2f716a8525b3f18608f2bdb800698a60e97133a
Kiko <kristiqnenchevv@gmail.com>
2023-06-07 11:16:30 +0300

    Add repository and MongoClient

--- DotNetServer/Infrastructure/Persistence/Startup.cs
+++ DotNetServer/Infrastructure/Persistence/Startup.cs
@@ -3,10 +3,39 @@
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
 
+    using MongoDB.Driver;
+
+    using Persistence.Repository;
+
     public static class Startup
     {
         public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
         {
+            services
+                .AddMongoClient(configuration)
+                .AddRepositories();
+
+            return services;
+        }
+
+        private static IServiceCollection AddMongoClient(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            services.AddSingleton<IMongoClient, MongoClient>(sp => new MongoClient(connectionString));
+            services.AddScoped<IMongoDatabase>(sp =>
+            {
+                var client = sp.GetRequiredService<IMongoClient>();
+                return client.GetDatabase("TodoSwitch");
+            });
+
+            return services;
+        }
+
+        private static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            services
+                .AddTransient(typeof(IMongoRepository<>), typeof(MongoRepository<>));
+
             return services;
         }
     }
