fc5b94ef0fccb17ddaae97c4107589fbab677400
Kiko <kristiqnenchevv@gmail.com>
2023-06-08 12:38:01 +0300

    Add Jwt Service

---
+++ DotNetServer/Core/Application/Interfaces/Services/IJwtService.cs
@@ -0,0 +1,11 @@
+﻿namespace Application.Interfaces.Services
+{
+    using Domain.Entities;
+
+    public interface IJwtService
+    {
+        Task<string> GenerateTokenAsync(User user, string ipAddress);
+
+        Task<string> GenerateRefreshToken(User user);
+    }
+}
---
+++ DotNetServer/Infrastructure/Infrastructure/Services/Token/JwtService.cs
@@ -0,0 +1,86 @@
+﻿namespace Infrastructure.Services.Token
+{
+    using System.Text;
+    using System.Security.Claims;
+    using System.IdentityModel.Tokens.Jwt;
+
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.Extensions.Options;
+    using Microsoft.IdentityModel.Tokens;
+
+    using Domain.Entities;
+
+    using Models;
+
+    using Shared.Exceptions;
+
+    using Application.Interfaces.Services;
+
+    public class JwtService : IJwtService
+    {
+        private readonly UserManager<User> userManager;
+        private readonly TokenSettings tokenSettings;
+
+        public JwtService(UserManager<User> UserManager, IOptions<TokenSettings> TokenSettings)
+        {
+            userManager = UserManager;
+            tokenSettings = TokenSettings.Value;
+        }
+
+        public async Task<string> GenerateTokenAsync(User user, string ipAddress)
+        {
+            var claimTypeFirstName = "FirstName";
+            var claimTypeLastName = "LastName";
+
+            var userRoles = await userManager.GetRolesAsync(user);
+            var roleClaims = new List<Claim>();
+
+            for (int i = 0; i < userRoles.Count; i++)
+            {
+                roleClaims.Add(new Claim("roles", userRoles[i]));
+            }
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Email, user.Email!),
+                new Claim(claimTypeFirstName, user.FirstName!),
+                new Claim(claimTypeLastName, user.LastName!),
+                new Claim(ClaimTypes.Name, user.UserName!),
+                new Claim("ip", ipAddress)
+            }
+            .Union(roleClaims);
+
+            var secret = Encoding.UTF8.GetBytes(tokenSettings.Key);
+
+            var token = new JwtSecurityToken(
+                issuer: tokenSettings.Issuer,
+                audience: tokenSettings.Audience,
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(tokenSettings.DurationInMinutes),
+                signingCredentials: new SigningCredentials(
+                    new SymmetricSecurityKey(secret),
+                    SecurityAlgorithms.HmacSha256));
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var encryptedToken = tokenHandler.WriteToken(token);
+
+            return encryptedToken;
+        }
+
+        public async Task<string> GenerateRefreshToken(User user)
+        {
+            await userManager.RemoveAuthenticationTokenAsync(user, "TodoSwitch", "RefreshToken");
+            var newRefreshToken = await userManager.GenerateUserTokenAsync(user, "TodoSwitch", "RefreshToken");
+            IdentityResult result = await userManager.SetAuthenticationTokenAsync(user, "TodoSwitch", "RefreshToken", newRefreshToken);
+
+            if (!result.Succeeded)
+            {
+                var errors = result.Errors.Select(e => e.Description);
+                throw new CustomException($"{string.Join(Environment.NewLine, errors)}");
+            }
+
+            return newRefreshToken;
+        }
+    }
+}
