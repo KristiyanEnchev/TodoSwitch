aa6ab95da485b026771b745c1c59c73b62be3053
Kiko <kristiqnenchevv@gmail.com>
2023-06-11 14:44:03 +0300

    Add Todo entities

---
+++ DotNetServer/Core/Domain/Entities/TodoList.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Entities
+{
+    using Domain.Common;
+
+    public class TodoList : BaseAuditableEntity
+    {
+        public string? Title { get; set; }
+        public Colour Colour { get; set; } = Colour.White;
+
+        public IList<TodoItem> Items { get; private set; } = new List<TodoItem>();
+    }
+}
