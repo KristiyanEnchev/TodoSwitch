cc0a62085cee90f12c9452317dafbfdba289f058
Kiko <kristiqnenchevv@gmail.com>
2023-06-22 14:30:40 +0300

    Update vite config

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tailwindcss from 'tailwindcss';
 
-// https://vitejs.dev/config/
+import fs from 'fs';
+
+const folders = fs.readdirSync('./src', { withFileTypes: true });
+const fileNames = folders
+  .filter((dirent) => dirent.isDirectory())
+  .map((dirent) => dirent.name);
+
+const filePaths = fileNames.reduce(
+  (acc, cur) => ({
+    ...acc,
+    [cur]: `/src/${cur}`,
+  }),
+  {}
+);
+
 export default defineConfig({
   plugins: [react()],
-})
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  },
+  resolve: {
+    alias: {
+      ...filePaths,
+    },
+  },
+});
