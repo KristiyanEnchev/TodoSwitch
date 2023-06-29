b29dec93a8dcb55bea25d91514b9f929c982d6ff
Kiko <kristiqnenchevv@gmail.com>
2023-06-29 11:39:01 +0300

    Apply Error boundry main layout and toaster configuration

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,33 +1,20 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { Helmet } from 'react-helmet-async';
+import { ToastContainer } from 'react-toastify';
+import MainLayout from 'layouts/MainLayout';
+import AppRouter from './Router';
+import ErrorBoundary from 'components/error/ErrorBoundary';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <ErrorBoundary>
+      <Helmet>
+        <title>TodoSwitch</title>
+      </Helmet>
+      <ToastContainer position="top-center" />
+      <MainLayout>
+        <AppRouter />
+      </MainLayout>
+    </ErrorBoundary>
   );
 }
 
