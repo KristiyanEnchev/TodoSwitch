5f16aeeaaa6f1960e1e9fa995bbd92b75ad03d12
Kiko <kristiqnenchevv@gmail.com>
2023-06-30 13:27:14 +0300

    Apply the main layout

---
+++ Client/src/components/index.jsx
@@ -0,0 +1,6 @@
+// eslint-disable-next-line react-refresh/only-export-components
+export { default as SEO } from './SEO/SEO.jsx';
+export { default as Spinner } from './spinner/Spinner.jsx';
+export { default as ErrorBoundary } from './error/ErrorBoundary';
+export { default as ProtectedRoute } from './auth/ProtectedRoute.jsx';
+export { default as PublicRoute } from './auth/PublicRoute.jsx';
--- Client/src/layouts/MainLayout.jsx
+++ Client/src/layouts/MainLayout.jsx
@@ -1,5 +1,30 @@
-const MainLayout = () => {
-  return <div>MainLayout</div>;
+import PropTypes from 'prop-types';
+import { SEO } from 'components';
+
+const MainLayout = ({ children }) => (
+  <>
+    <SEO
+      title="TodoSwitch"
+      description="To do app project"
+      name="TodoSwitch."
+      type="article"
+      url="https://kristiyan-enchev-website.web.app/"
+      twitterDomain="https://kristiyan-enchev-website.web.app/"
+      themeColor="#232e58"
+      author="Kristiyan Enchev"
+      keywords="React, JavaScript, Todo App"
+      robots="index, follow"
+      locale="en_US"
+      siteName="Todo Switch"
+    />{' '}
+    <div className="flex flex-col min-h-screen">
+      <main className="flex-grow bg-gray-300 dark:bg-black">{children}</main>
+    </div>
+  </>
+);
+
+MainLayout.propTypes = {
+  children: PropTypes.node.isRequired,
 };
 
 export default MainLayout;
