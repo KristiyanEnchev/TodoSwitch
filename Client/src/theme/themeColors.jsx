267d47e1bc7259c1383429070d332f7d4d6dc552
Kiko <kristiqnenchevv@gmail.com>
2023-07-03 12:58:01 +0300

    Add theme colors

---
+++ Client/src/theme/themeColors.jsx
@@ -0,0 +1,6 @@
+export const themeColors = [
+  { name: 'myTeal', color: '#008080' },
+  { name: 'myOrange', color: '#ffa500' },
+  { name: 'myRed', color: '#dc2626' },
+  { name: 'myGreen', color: '#008000' },
+];
