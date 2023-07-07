175c043e65dd4280afb9122bcf183f4be1f6b88a
Kiko <kristiqnenchevv@gmail.com>
2023-07-07 11:31:27 +0300

    Create a form field component

---
+++ Client/src/components/form/FormField.jsx
@@ -0,0 +1,58 @@
+import { useState, memo } from 'react';
+import PropTypes from 'prop-types';
+import PasswordStrengthBar from 'react-password-strength-bar';
+
+const FormField = memo(
+  ({ label, type, value, onChange, onFocus, error, ...props }) => {
+    const [showPassword, setShowPassword] = useState(false);
+    const isPasswordField = type === 'password' || type === 'confirmPassword';
+    const inputType = isPasswordField
+      ? showPassword
+        ? 'text'
+        : 'password'
+      : type;
+    const isPassword = type === 'password';
+
+    const togglePasswordVisibility = () => setShowPassword((prev) => !prev);
+
+    return (
+      <div className="mb-2">
+        <div className="relative">
+          <label className="text-gray-400 block mb-1">{label}</label>
+          <input
+            type={inputType}
+            value={value}
+            onChange={onChange}
+            onFocus={onFocus}
+            className="text-white rounded-lg bg-gray-700 w-full p-2 focus:bg-gray-500 focus:outline-none"
+            {...props}
+          />
+          {isPasswordField && (
+            <button
+              onClick={togglePasswordVisibility}
+              type="button"
+              className="absolute right-3 top-10 text-xs text-gray-400"
+            >
+              {showPassword ? 'Hide' : 'Show'}
+            </button>
+          )}
+          {isPassword && <PasswordStrengthBar password={value} />}
+          {error && <span className="text-redExtra text-sm">{error}</span>}
+        </div>
+      </div>
+    );
+  }
+);
+
+FormField.displayName = 'FormField';
+
+FormField.propTypes = {
+  label: PropTypes.string.isRequired,
+  type: PropTypes.string,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+  onFocus: PropTypes.func,
+  error: PropTypes.string,
+};
+
+export default FormField;
