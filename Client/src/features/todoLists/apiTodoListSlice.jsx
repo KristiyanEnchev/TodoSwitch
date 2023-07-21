87f7a658d3aa3d0f07ec072ccaaee45570a09b65
Kiko <kristiqnenchevv@gmail.com>
2023-07-21 14:20:19 +0300

    Add all api calls related to todo lists and colors

--- Client/src/features/todoLists/apiTodoListSlice.jsx
+++ Client/src/features/todoLists/apiTodoListSlice.jsx
@@ -1,5 +1,5 @@
 import { apiSlice } from '../../api/apiSlice.jsx';
-import { setTodoLists } from '../todoLists/todoListSlice.jsx';
+import { setTodoLists, updateTodoList } from '../todoLists/todoListSlice.jsx';
 import { setColors } from '../todoLists/colorsSlice.jsx';
 
 export const todoListsApiSlice = apiSlice.injectEndpoints({
@@ -32,8 +32,81 @@ export const todoListsApiSlice = apiSlice.injectEndpoints({
         }
       },
     }),
+    createTodoList: builder.mutation({
+      query: (listCommand) => ({
+        url: '/todolist/create',
+        method: 'POST',
+        body: listCommand,
+      }),
+      invalidatesTags: ['TodoList'],
+    }),
+    updateList: builder.mutation({
+      query: (listCommand) => ({
+        url: '/todolist/updatelist',
+        method: 'PUT',
+        body: listCommand,
+      }),
+      invalidatesTags: ['TodoList'],
+      async onQueryStarted(arg, { dispatch, queryFulfilled }) {
+        try {
+          const { data } = await queryFulfilled;
+          dispatch(updateTodoList(data));
+        } catch (error) {
+          console.error('Error updating todo list:', error);
+        }
+      },
+    }),
+    updateTodoListTitle: builder.mutation({
+      query: (titleCommand) => ({
+        url: '/todolist/updatetitle',
+        method: 'PUT',
+        body: titleCommand,
+      }),
+      invalidatesTags: ['TodoList'],
+    }),
+    updateTodoListColor: builder.mutation({
+      query: (colorCommand) => ({
+        url: '/todolist/updatecolor',
+        method: 'PUT',
+        body: colorCommand,
+      }),
+      invalidatesTags: ['TodoList'],
+    }),
+    changeTodoListOrder: builder.mutation({
+      query: (orderCommand) => ({
+        url: '/todolist/changeorder',
+        method: 'PUT',
+        body: orderCommand,
+      }),
+      invalidatesTags: ['TodoList'],
+    }),
+    deleteTodoList: builder.mutation({
+      query: ({ userId, listId }) => ({
+        url: '/todolist/delete',
+        method: 'DELETE',
+        params: { userId, listId },
+      }),
+      invalidatesTags: ['TodoList'],
+    }),
+    reorderList: builder.mutation({
+      query: (orderCommand) => ({
+        url: '/todolist/reorder',
+        method: 'PUT',
+        body: orderCommand,
+      }),
+      invalidatesTags: ['TodoList'],
+    }),
   }),
 });
 
-export const { useGetAllTodoListsQuery, useGetTodoListColorsQuery } =
-  todoListsApiSlice;
+export const {
+  useGetAllTodoListsQuery,
+  useGetTodoListColorsQuery,
+  useCreateTodoListMutation,
+  useUpdateListMutation,
+  useUpdateTodoListTitleMutation,
+  useUpdateTodoListColorMutation,
+  useDeleteTodoListMutation,
+  useChangeTodoListOrderMutation,
+  useReorderListMutation,
+} = todoListsApiSlice;
