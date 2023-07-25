c32903a5a66ed8b98e3e21c626a004d6fa20a482
Kiko <kristiqnenchevv@gmail.com>
2023-07-25 13:09:02 +0300

    Extend the todo item api slice with all the api calls provided from the api

--- Client/src/features/todoItems/todoItemsApiSlice.jsx
+++ Client/src/features/todoItems/todoItemsApiSlice.jsx
@@ -1,5 +1,5 @@
 import { apiSlice } from '../../api/apiSlice.jsx';
-import { setTodoItems } from '../todoItems/todoItemSlice.jsx';
+import { setTodoItems, updateTodoItem } from '../todoItems/todoItemSlice.jsx';
 
 export const todoItemsApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
@@ -18,7 +18,105 @@ export const todoItemsApiSlice = apiSlice.injectEndpoints({
         }
       },
     }),
+    getTodoItemById: builder.query({
+      query: ({ userId, listId, itemId }) => ({
+        url: '/todoitem/getbyid',
+        params: { userId, listId, itemId },
+      }),
+      providesTags: (result, error, arg) => [
+        { type: 'TodoItem', id: arg.itemId },
+      ],
+      async onQueryStarted(arg, { dispatch, queryFulfilled }) {
+        try {
+          const { data } = await queryFulfilled;
+          dispatch(updateTodoItem(data));
+        } catch (error) {
+          console.error('Error fetching todo item by ID:', error);
+        }
+      },
+    }),
+    createTodoItem: builder.mutation({
+      query: (todoItem) => ({
+        url: '/todoitem/createtodo',
+        method: 'POST',
+        body: todoItem,
+      }),
+      invalidatesTags: ['TodoItem', 'TodoList'],
+    }),
+    toggleTodoStatus: builder.mutation({
+      query: (toggleCommand) => ({
+        url: '/todoitem/togglestatus',
+        method: 'PUT',
+        body: toggleCommand,
+      }),
+      invalidatesTags: (result, error, arg) => [
+        { type: 'TodoItem', id: arg.itemId },
+        'TodoList',
+      ],
+    }),
+    changeTodoPriority: builder.mutation({
+      query: (priorityCommand) => ({
+        url: '/todoitem/changepriority',
+        method: 'PUT',
+        body: priorityCommand,
+      }),
+      invalidatesTags: (result, error, arg) => [
+        { type: 'TodoItem', id: arg.itemId },
+      ],
+    }),
+    changeTodoOrder: builder.mutation({
+      query: (orderCommand) => ({
+        url: '/todoitem/changeorder',
+        method: 'PUT',
+        body: orderCommand,
+      }),
+      invalidatesTags: ['TodoItem'],
+    }),
+    reorder: builder.mutation({
+      query: (orderCommand) => ({
+        url: '/todoitem/reorder',
+        method: 'PUT',
+        body: orderCommand,
+      }),
+      invalidatesTags: ['TodoItem'],
+    }),
+    updateTodoItem: builder.mutation({
+      query: (itemCommand) => ({
+        url: '/todoitem/updateitem',
+        method: 'PUT',
+        body: itemCommand,
+      }),
+      invalidatesTags: (result, error, arg) => [
+        { type: 'TodoItem', id: arg.itemId },
+      ],
+    }),
+    deleteTodoItem: builder.mutation({
+      query: ({ userId, listId, itemId }) => ({
+        url: '/todoitem/delete',
+        method: 'DELETE',
+        params: { userId, listId, itemId },
+      }),
+      invalidatesTags: ['TodoItem', 'TodoList'],
+    }),
+    changeTodoItemOrder: builder.mutation({
+      query: (orderCommand) => ({
+        url: '/todoitem/changeorder',
+        method: 'PUT',
+        body: orderCommand,
+      }),
+      invalidatesTags: ['TodoItem'],
+    }),
   }),
 });
 
-export const { useGetPagedTodosForUserQuery } = todoItemsApiSlice;
+export const {
+  useGetPagedTodosForUserQuery,
+  useGetTodoItemByIdQuery,
+  useCreateTodoItemMutation,
+  useToggleTodoStatusMutation,
+  useChangeTodoPriorityMutation,
+  useUpdateTodoItemMutation,
+  useDeleteTodoItemMutation,
+  useChangeTodoItemOrderMutation,
+  useReorderMutation,
+} = todoItemsApiSlice;
