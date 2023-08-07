c0893552475fc4096748d894e064521b9bbd1e9b
Kiko <kristiqnenchevv@gmail.com>
2023-08-07 11:14:49 +0300

    Update fetures index.jsx

--- Client/src/features/index.jsx
+++ Client/src/features/index.jsx
@@ -4,3 +4,22 @@ export {
   useLogoutMutation,
 } from './auth/authApiSlice.jsx';
 export { setCredentials, logOut } from './auth/authSlice.jsx';
+export { openModal, closeModal } from './modal/modalSlice.jsx';
+export { setColors, setSelectedColor } from './todoLists/colorsSlice.jsx';
+export {
+  setSelectedList,
+  updateSelectedList,
+} from './todoLists/selectedListSlice.jsx';
+export { setTodoLists, updateTodoList } from './todoLists/todoListSlice.jsx';
+export {
+  useGetAllTodoListsQuery,
+  useGetTodoListColorsQuery,
+  useCreateTodoListMutation,
+  useUpdateListMutation,
+  useUpdateTodoListTitleMutation,
+  useUpdateTodoListColorMutation,
+  useDeleteTodoListMutation,
+  useChangeTodoListOrderMutation,
+} from './todoLists/apiTodoListSlice.jsx';
+
+export { setTodoItems, updateTodoItem } from './todoItems/todoItemSlice.jsx';
