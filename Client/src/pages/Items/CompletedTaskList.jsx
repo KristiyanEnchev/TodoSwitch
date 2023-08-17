ab5b125c92b5b2d23a5aead1185f563c9f4e0bfb
Kiko <kristiqnenchevv@gmail.com>
2023-08-17 11:54:19 +0300

    Create a Completed tasks section

---
+++ Client/src/pages/Items/CompletedTaskList.jsx
@@ -0,0 +1,50 @@
+import { memo, useState, useCallback } from 'react';
+import PropTypes from 'prop-types';
+import TaskItem from './TaskItem.jsx';
+import { ChevronDown, ChevronRight } from 'lucide-react';
+import Button from '../../components/ui/Button.jsx';
+
+const CompletedTaskList = ({ tasks, onToggle, onDelete, onEdit }) => {
+  const [open, setOpen] = useState(false);
+
+  const handleToggleOpen = useCallback(() => setOpen((prev) => !prev), []);
+
+  return (
+    <div className="mt-4">
+      <Button
+        variant="outline"
+        className="w-full justify-between"
+        onClick={handleToggleOpen}
+      >
+        <span>Completed ({tasks.length})</span>
+        {open ? (
+          <ChevronDown className="h-4 w-4" />
+        ) : (
+          <ChevronRight className="h-4 w-4" />
+        )}
+      </Button>
+      {open && (
+        <ul className="mt-2">
+          {tasks.map((task) => (
+            <TaskItem
+              key={task.id}
+              task={task}
+              onToggle={onToggle}
+              onDelete={onDelete}
+              onEdit={onEdit}
+            />
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+CompletedTaskList.propTypes = {
+  tasks: PropTypes.array.isRequired,
+  onToggle: PropTypes.func.isRequired,
+  onDelete: PropTypes.func.isRequired,
+  onEdit: PropTypes.func.isRequired,
+};
+
+export default memo(CompletedTaskList);
